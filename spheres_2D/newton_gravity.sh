9ff9abacaa32d780bc7ab6c87b85085fc6a5fdf5
HanNotSoSolo <101388498+HanNotSoSolo@users.noreply.github.com>
2025-10-10 11:08:53 +0200

    Added necessary SLURM Bash files.
    These files will be used to launch jobs via the supercomputer.

---
+++ spheres_2D/newton_gravity.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=newt_2D
+#SBATCH --time=2-00:00
+#SBATCH --mem=120G
+#SBATCH --qos=c3_long_opa
+#SBATCH --output=%j.spheres_2D_newton.out
+#SBATCH --profile=all
+
+echo "Starting Newton calculation..."
+srun python /scratchm/mdellava/femtoscope/spheres_2D/newton_gravity.py
+wait
---
+++ spheres_2D/yukawa_gravity.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=yuka_2D
+#SBATCH --time=2-00:00
+#SBATCH --mem=150G
+#SBATCH --qos=c3_long_opa
+#SBATCH --output=%j.spheres_2D_yukawa.out
+#SBATCH --profile=all
+
+echo "Starting Yukawa calculation..."
+srun python /scratchm/mdellava/femtoscope/spheres_2D/yukawa_gravity.py
+wait
+
+echo "Calculation terminated, launching Newton's calculation and quitting."
+sbatch $WORKDIR/femtoscope/spheres_2D/newton_gravity.sh
